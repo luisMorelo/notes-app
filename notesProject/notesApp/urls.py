ee195f473d3a2ea04e28fb881253b365b6749871
Luis Morelo <luisalmopi@gmail.com>
2024-12-05 22:02:37 -0500

    Se agrago la app rest_framework.authtoken en settings

--- notesProject/notesApp/urls.py
+++ notesProject/notesApp/urls.py
@@ -4,4 +4,5 @@ from .import views
 urlpatterns = [
     path('put/api/notes', views.NotesCreate.as_view()),
     path('get/api/notes', views.NotesList.as_view())
+    
 ]
--- notesProject/notesProject/settings.py
+++ notesProject/notesProject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'notesApp',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
