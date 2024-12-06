b94ddca09e6468c54cf6f564a7c5d6d1fa28d7b8
Luis Morelo <luisalmopi@gmail.com>
2024-12-05 21:04:09 -0500

    se creo vistas basadas en clase y platilla .env.example

--- notesProject/notesApp/views.py
+++ notesProject/notesApp/views.py
@@ -5,6 +5,27 @@ from rest_framework import generics
 
 #--------- Vistas vasadas en clase para el modelo notas ------
 
+#Para listar notas
 class NotesList(generics.ListAPIView):
     queryset = Notes.objects.all()
     serializer_class = notesSerializers 
+
+#para crear notas
+class NotesCreate(generics.CreateAPIView):
+    queryset = Notes.objects.all()
+    serializer_class = notesSerializers
+
+#ver un pbjeto unico pot ID
+class TaskDetail(generics.RetrieveAPIView):
+    queryset = Notes.objects.all()
+    serializer_class = notesSerializers
+
+#Actualizar notas
+class TaskUpdate(generics.UpdateAPIView):
+    queryset = Notes.objects.all()
+    serializer_class = notesSerializers
+
+#Borar un registro 
+class TaskDelete(generics.DestroyAPIView):
+    queryset = Notes.objects.all()
+    serializer_class = notesSerializers
