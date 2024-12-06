af630595dbff305cbd2350ec025c7838fc2b8ad2
Luis Morelo <luisalmopi@gmail.com>
2024-12-06 11:52:37 -0500

    app toquen para usuario funcionando

--- notesProject/notesApp/urls.py
+++ notesProject/notesApp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .import views 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('put/api/notes', views.NotesCreate.as_view()),
-    path('get/api/notes', views.NotesList.as_view())
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     
+    path('put/api/notes', views.NotesCreate.as_view()),
+    path('get/api/notes', views.NotesList.as_view()),
 ]
--- notesProject/notesApp/views.py
+++ notesProject/notesApp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Notes
 from .serializers import notesSerializers
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 #--------- Vistas vasadas en clase para el modelo notas ------
 
@@ -9,23 +9,28 @@ from rest_framework import generics
 class NotesList(generics.ListAPIView):
     queryset = Notes.objects.all()
     serializer_class = notesSerializers 
-
+    permission_classes = [permissions.IsAuthenticated]  # Requiere autenticación
+    
 #para crear notas
 class NotesCreate(generics.CreateAPIView):
     queryset = Notes.objects.all()
     serializer_class = notesSerializers
+    permission_classes = [permissions.IsAuthenticated]  # Requiere autenticación
 
 #ver un objeto unico pot ID
 class TaskDetail(generics.RetrieveAPIView):
     queryset = Notes.objects.all()
     serializer_class = notesSerializers
+    permission_classes = [permissions.IsAuthenticated]  # Requiere autenticación
 
 #Actualizar notas
 class TaskUpdate(generics.UpdateAPIView):
     queryset = Notes.objects.all()
     serializer_class = notesSerializers
+    permission_classes = [permissions.IsAuthenticated]  # Requiere autenticación
 
 #Borar un registro 
 class TaskDelete(generics.DestroyAPIView):
     queryset = Notes.objects.all()
     serializer_class = notesSerializers
+    permission_classes = [permissions.IsAuthenticated]  # Requiere autenticación
