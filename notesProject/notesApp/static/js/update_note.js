1bb0a2bcaab26e77b39bc1919616a1a19a01c3b1
Luis Morelo <luisalmopi@gmail.com>
2024-12-10 22:34:24 -0500

    update_note.js para bloqueo optimista

---
+++ notesProject/notesApp/static/js/update_note.js
@@ -0,0 +1,34 @@
+document.getElementById("saveNoteButton").addEventListener("click", function () {
+    const noteId = document.getElementById("noteId").value;
+    const titulo = document.getElementById("titulo").value;
+    const contenido = document.getElementById("contenido").value;
+    const terminada = document.getElementById("terminada").checked; // Adaptado para el checkbox
+
+    fetch("update-note/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value,
+        },
+        body: JSON.stringify({
+            note_id: noteId,
+            titulo: titulo,
+            contenido: contenido,
+            terminada: terminada,
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.error) {
+                document.getElementById("errorMessage").innerText = data.error;
+                document.getElementById("errorMessage").style.display = "block";
+            } else {
+                alert("Nota actualizada correctamente");
+                window.location.reload(); // Recargar la página para reflejar cambios
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+            alert("Ocurrió un error inesperado.");
+        });
+});
