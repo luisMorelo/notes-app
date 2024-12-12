a78b4b5b8f43431bd3dd6246a7f7f95ac7a8e97a
Luis Morelo <luisalmopi@gmail.com>
2024-12-11 22:54:29 -0500

    Se agregó el campo version al modelo

--- notesProject/notesApp/models.py
+++ notesProject/notesApp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
 class Notes(models.Model):
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
     fecha_creacion = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  # Fecha de última actualización
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    updated_at = models.DateTimeField(auto_now=True)  # La fecha cuando se actualiza
+    version = models.PositiveIntegerField(default=0)  # Campo de versión para el bloqueo optimista
 
     def __str__(self):
         return self.titulo
